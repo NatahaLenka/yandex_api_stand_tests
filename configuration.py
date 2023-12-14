e21ee9f9ca9d1b99c3aa7e4c1a9a1fa0f6d2223f
Egor Okulov <eokulov37@gmail.com>
2023-12-14 19:21:21 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://a967767c-5290-40e4-88ad-10f43823d107.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
